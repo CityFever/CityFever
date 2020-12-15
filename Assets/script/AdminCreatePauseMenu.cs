8ef6509ac6e1f87d37e5f11ceafa295ab40d029d
224088 <224088@edu.p.lodz.pl>
2020-12-15 18:46:14 +0100

    gui integration

---
+++ Assets/script/AdminCreatePauseMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AdminCreatePauseMenu : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("AdminMenu");
+
+    }
+}
---
+++ GameUI/Assets/script/LoginToMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class LoginToMenu : MonoBehaviour
+{
+    public void goToAdminMenu()
+    {
+        SceneManager.LoadScene("AdminMenu");
+    }
+
+    public void goToPlayerMenu()
+    {
+        SceneManager.LoadScene("PlayerMenu");
+    }
+
+
+}
