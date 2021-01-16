bb259ccc286bd5fc1bf71e2316e6afff95a94daf
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-01-17 00:28:14 +0100

    removed unnecessary function

--- Assets/AdminMap/Scripts/GamePanel/AdminGameManager.cs
+++ Assets/AdminMap/Scripts/GamePanel/AdminGameManager.cs
@@ -44,21 +44,11 @@ public class AdminGameManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (!EventSystem.current.IsPointerOverGameObject())
+            if (!EventSystem.current.IsPointerOverGameObject()) //block clicks on gui elements
             {
                 SelectTileOnMouseClick();
                 SelectObjectOnMouseClick();
             }
-            /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (!IsPointerOverUIObject())
-                {
-                    SelectTileOnMouseClick();
-                    SelectObjectOnMouseClick();
-                }
-            }*/
         }
 
         else if (showHover && !mode.Equals(GameMode.Default))
@@ -386,15 +376,5 @@ public class AdminGameManager : MonoBehaviour
             prefab => prefab.Type().Equals(selectedType));
         Debug.Log("Instantiated: " + prefabToInstantiate.Type());
         SetGameObjectPrefab(prefabToInstantiate);
-    }
-
-    //When Touching UI
-    private bool IsPointerOverUIObject()
-    {
-        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
-        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
-        return results.Count > 0;
     }
 }
